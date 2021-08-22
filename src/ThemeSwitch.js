6b41403cd3a081f2f9dbc37b92d320e8f25dd4eb
Sinan <logistus@gmail.com>
2021-08-23 01:13:42 +0300

    bg pattern added

--- src/ThemeSwitch.js
+++ src/ThemeSwitch.js
@@ -5,16 +5,17 @@ import {ThemeContext} from "./Provider";
 const ThemeSwitch = () => {
   const { theme, setTheme } = useContext(ThemeContext);
   return (
-    <label>
-    <Switch
-      checked={theme === "dark" ? false : true}
-      onChange={setTheme}
-      checkedIcon={false}
-      uncheckedIcon={false}
-      handleDiameter={20}
-      onColor="#AEB3CB"
-      />
-      </label>
+    <label htmlFor="theme-switch">
+      <Switch
+        checked={theme === "dark" ? false : true}
+        onChange={setTheme}
+        checkedIcon={false}
+        uncheckedIcon={false}
+        handleDiameter={20}
+        onColor="#AEB3CB"
+        id="theme-switch"
+        />
+    </label>
   );
 }
 
